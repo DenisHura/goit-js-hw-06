04e3c9bc886e568215300a77f7b6a10dbaedae57
DenisHura <hura.dv88@gmail.com>
2022-07-16 23:19:42 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,14 +9,23 @@ const ingredients = [
 
 
 const list = document.querySelector("#ingredients")
-const createItem = (array) => {
-  for (let i = 0; i < array.length; i += 1) {
-    const item = document.createElement("li");
-    item.classList.add("item")
-    item.textContent = array[i]
-    console.log(item)
-  }
-}
+// const createItem = (array) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     const item = document.createElement("li");
+//     item.classList.add("item")
+//     item.textContent = array[i]
+//     console.log(item)
+//   }
+  
+// }
 
-createItem(ingredients)
+// createItem(ingredients)
 
+
+const ingredientsList = ingredients.map(ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+    return item;
+})
+list.append(...ingredientsList)
